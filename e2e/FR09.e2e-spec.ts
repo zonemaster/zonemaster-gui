34c29279b11d4b73e2f298dd3a8c90e1e290fa90
Pierre-Aymeric Masse <pamasse@afnic.fr>
2019-10-28 20:49:26 +0100

    fix FR09.e2e

--- e2e/FR09.e2e-spec.ts
+++ e2e/FR09.e2e-spec.ts
@@ -11,7 +11,7 @@ describe('Zonemaster test FR09 - [Once a language is chosen, all other links sho
 
   it('should keep french when opening faq page', async() => {
     await browser.wait(() => element(by.xpath('//h1[.="Nom de domaine"]')).isPresent(), 21 * 1000);
-    await utils.goTo('faq');
+    await element(by.css('a.nav-link[routerlink="/faq"]')).click();
     await expect(element(by.css('section.main > div > h1')).getText())
       .toBe('FAQ');
     await expect(element(by.css('a.nav-link[routerlink="/domain_check"]')).getText()).toBe('Test d\'un domaine');
