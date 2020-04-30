4eee6f116c11f2318578959f3228e0f6ea2c1cb7
Mats Dufberg <mats.dufberg@iis.se>
2020-04-30 20:38:18 +0200

    Test 4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   apiEndpoint: 'https://zonemaster.net/api',
   contactAddress: 'contact@zonemaster.net',
   logoUrl: 'assets/images/zonemaster_logo_2020_color.svg',
-  clientInfo: {version: '3.1.0', id: 'Zonemaster GUI'},
+  clientInfo: {version: '3.2.0', id: 'Zonemaster GUI'},
   mock: true
 };
