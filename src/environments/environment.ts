89caa590c76ed5186592ec7be5a0434491e4878b
jitendra.mandal <jitendra.mandal@stigasoft.com>
2020-12-03 17:38:18 +0530

    design changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'https://zonemaster.net/api',
+  apiEndpoint: 'http://zonemaster.demolinks.xyz/api',
   contactAddress: 'contact@zonemaster.net',
   logoUrl: 'assets/images/zonemaster_logo_2020_color.svg',
   clientInfo: {version: '3.2.1', id: 'Zonemaster GUI'},
