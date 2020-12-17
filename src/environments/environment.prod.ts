a4eb89ae196243b237f4f084a788fbb1368de8ab
Mats Dufberg <mats.dufberg@iis.se>
2020-12-17 20:00:03 +0100

    Updated with new logotype (2021)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiEndpoint: '/api',
   contactAddress: 'contact@zonemaster.net',
-  logoUrl: 'assets/images/zonemaster_logo_2020_color.svg',
+  logoUrl: 'assets/images/zonemaster_logo_2021_color.svg',
   clientInfo: {version: '3.2.1', id: 'Zonemaster GUI'},
   mock: false
 };
