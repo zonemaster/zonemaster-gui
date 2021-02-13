e2fda54aa23b687ec54ccd7469d3301cb564f2c0
Mats Dufberg <mats.dufberg@iis.se>
2021-02-13 14:49:51 +0100

    Adds setting for including Finnish language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,9 +30,10 @@ import { HeaderComponent } from './components/header/header.component';
 import {SafeHtmlPipe} from './pipes/safe-html.pipe';
 import { MomentModule } from 'ngx-moment';
 
+import 'moment/locale/da';
+import 'moment/locale/fi';
 import 'moment/locale/fr';
 import 'moment/locale/sv';
-import 'moment/locale/da';
 
 import { HttpRequestInterceptor } from './interceptors/request.interceptor';
 import { HttpMockRequestInterceptor } from './interceptors/mock.interceptor';
