c44532f64a62527cb9c1a16c6b5ae3486b9a92f4
Mats Dufberg <mats.dufberg@iis.se>
2021-04-12 17:26:20 +0200

    test

--- e2e/FR05fi.e2e-spec.ts
+++ e2e/FR05fi.e2e-spec.ts
@@ -2,7 +2,7 @@ import { by, browser, element } from 'protractor';
 
 import { Utils } from './utils/app.utils';
 
-describe('Zonemaster test FR05 - [Supports Finnish language]', () => {
+describe('Zonemaster test FR05fi - [Supports Finnish language]', () => {
   const utils = new Utils();
   beforeAll(() => {
     utils.goToHome();
@@ -12,8 +12,8 @@ describe('Zonemaster test FR05 - [Supports Finnish language]', () => {
     expect(element(by.xpath('//a[@lang="fi"]')).isPresent()).toBe(true);
   });
 
-  it('should switch to Swedish', () => {
-    utils.setLang('sv');
+  it('should switch to Finnish', () => {
+    utils.setLang('fi');
     expect(element(by.xpath('//h1[.="Verkkotunnus"]')).isPresent()).toBe(true);
   });
 });
