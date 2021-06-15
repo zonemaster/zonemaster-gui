6a575dfee7821843c57e2f1058d06bca7188f008
Gaël Berthaud-Müller <gael.berthaud-muller@afnic.fr>
2021-06-15 14:28:27 +0200

    add sleep

--- e2e/FR16.e2e-spec.ts
+++ e2e/FR16.e2e-spec.ts
@@ -18,10 +18,13 @@ describe('Zonemaster test FR16 - [The advanced view should have a text describin
 
   it('should have a description text in multi languages', async () => {
     await utils.setLang('en');
+    await browser.sleep(100);
     expect(element(by.css('.alert.alert-info')).element(by.css('a')).getText()).toContain('undelegated');
     await utils.setLang('fr');
+    await browser.sleep(100);
     expect(element(by.css('.alert.alert-info')).element(by.css('a')).getText()).toContain('non délégué');
     await utils.setLang('sv');
+    await browser.sleep(100);
     expect(element(by.css('.alert.alert-info')).element(by.css('a')).getText()).toContain('odelegerat domäntest');
   });
 });
