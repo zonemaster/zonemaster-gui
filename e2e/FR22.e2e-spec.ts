303f7ca8af74b284a010ea08640ecf896f7f0793
Gaël Berthaud-Müller <gael.berthaud-muller@afnic.fr>
2021-06-30 17:14:00 +0200

    update tests

--- e2e/FR22.e2e-spec.ts
+++ e2e/FR22.e2e-spec.ts
@@ -18,11 +18,11 @@ describe('Zonemaster test FR22 - [Provide the possibility to see more informatio
     await element(by.css('#domain_check_name')).sendKeys('afNiC.Fr');
     await element(by.css('div button.launch')).click();
 
-    await browser.wait(() => element(by.css('#collapsed_module_1')).isPresent(), 120 * 1000);
+    await browser.wait(() => element(by.css('#collapsed_module_BASIC')).isPresent(), 120 * 1000);
 
-    await expect(element(by.css('#collapsed_module_1')).getText()).toEqual('BASIC');
-    await expect(element.all(by.css('#collapsed_module_1 > tr.show')).count()).toBe(0);
-    await element(by.css('#collapsed_module_1 > tr:nth-child(1)')).click();
-    await expect(element.all(by.css('#collapsed_module_1 > tr.show')).count()).toEqual(14);
+    await expect(element(by.css('#collapsed_module_BASIC')).getText()).toEqual('BASIC');
+    await expect(element.all(by.css('#collapsed_module_BASIC > tr.show')).count()).toBe(0);
+    await element(by.css('#collapsed_module_BASIC > tr:nth-child(1)')).click();
+    await expect(element.all(by.css('#collapsed_module_BASIC > tr.show')).count()).toEqual(14);
   });
 });
