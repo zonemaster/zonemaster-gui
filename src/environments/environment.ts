cfe1e4c44edff6f428c48ecdc4e673552845e4f9
Gaël Berthaud-Müller <gael.berthaud-muller@afnic.fr>
2021-08-04 11:37:21 +0200

    improve development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'https://zonemaster.net/api',
+  apiEndpoint: '/api',
   contactAddress: 'contact@zonemaster.net',
   logoUrl: 'assets/images/zonemaster_logo_2021_color.png',
   clientInfo: {version: '3.3.0', id: 'Zonemaster GUI'},
