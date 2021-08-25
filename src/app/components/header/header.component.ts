cb4deb6ccdb7da38f37b67bb3220ee36c0c3f175
Alexandre Pion <pion@afnic.fr>
2021-08-25 12:04:39 +0200

    Move components below the navbar
    Subscribe to the service to get the current height of the navbar and let
    the components stay below it.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  public navHeight: Number;
+  private navHeightSubscription: Subscription;
+
+  constructor(private navigationService: NavigationService) { }
 
   ngOnInit() {
+    this.navHeightSubscription = this.navigationService.height.subscribe((newHeight: Number) => {
+      this.navHeight = newHeight;
+    });
+  }
+
+  ngOnDestroy() {
+    this.navHeightSubscription.unsubscribe();
   }
 }
