bd1f8ab117357f729906ad1ff8ad3a6f9d6ac123
Gaël Berthaud-Müller <gael.berthaud-muller@afnic.fr>
2023-08-24 14:12:53 +0200

    finish faq behaviour

--- src/app/components/faq-question/faq-question.component.ts
+++ src/app/components/faq-question/faq-question.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, Input, OnChanges, ViewChild } from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-faq-question',
@@ -7,8 +8,38 @@ import {ViewEncapsulation} from '@angular/core';
   styleUrls: ['./faq-question.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class FaqQuestionComponent {
+export class FaqQuestionComponent implements OnChanges, AfterViewChecked {
+  @Input('questionId') questionId;
+  @ViewChild('entry') entryElement;
+  public isOpen: boolean = false;
+  private fragment: string = null;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.fragment = this.router.getCurrentNavigation().extractedUrl.fragment;
+  }
+
+  ngOnChanges() {
+    if (!this.isOpen && this.fragment !== null) {
+      this.isOpen = this.fragment === this.questionId;
+    }
+  }
+
+  ngAfterViewChecked() {
+    if (this.isOpen && this.fragment === this.questionId) {
+      this.entryElement.nativeElement.scrollIntoView();
+    }
+
+    this.fragment = null;
+  }
+
+  public toggleOpenEntry() {
+    if (!this.isOpen) {
+      this.router.navigate(['faq'], { fragment: this.questionId,  state: { scroll: false } });
+    } else {
+      this.router.navigate(['faq']);
+    }
+
+    this.isOpen = !this.isOpen;
+  }
 
 }
--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, AfterViewChecked, OnDestroy } from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Title } from '@angular/platform-browser';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-faq',
@@ -10,38 +11,12 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./faq.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class FaqComponent implements OnInit, OnDestroy, AfterViewChecked {
-  private fragment: string;
-  public faqTemplate = '';
-  public url = '';
+export class FaqComponent implements OnInit {
 
-  private fragmentSubscription: Subscription;
-
-  constructor(private route: ActivatedRoute,
-              private titleService: Title) {
+  constructor(private titleService: Title) {
   }
 
   ngOnInit() {
-    this.fragmentSubscription = this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
-  }
-
-  ngOnDestroy() {
-    this.fragmentSubscription.unsubscribe();
-  }
-
-  ngAfterViewChecked(): void {
-    if (!this.fragment) {
-      return
-    }
-
-    const faqEntry = document.getElementById(this.fragment);
-    if (faqEntry !== null) {
-      faqEntry.scrollIntoView();
-      this.fragment = '';
-    }
-  }
-
-  loadFaq() {
     this.titleService.setTitle(`${$localize `:@@zm.faq.title:FAQ`} · Zonemaster`);
   }
 }
