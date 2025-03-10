7bf0ae22bd7054205ea898cbb9dc775cbe8afe37
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2025-03-10 21:33:01 +0100

    Started with faq

---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const faqCollection = defineCollection({
+  schema: z.object({
+    question: z.string(),
+    category: z.string(),
+  }),
+});
+
+export const collections = {
+  faq: faqCollection,
+};
---
+++ src/lib/components/Collapsible/Collapsible.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import stack from '@/lib/components/Stack/stack.module.css';
+
+  type Props = {
+    id: string;
+    title: string;
+    content: string;
+  };
+
+  let {
+    id,
+    title,
+    content,
+  }: Props = $props();
+  let open = $state(false);
+
+  function onClick() {
+    open = !open;
+  }
+</script>
+<article class="zm-collapsible" id={id}>
+  <header>
+    <a class="{stack.stack} {stack.middle} {stack['gap--xs']}" onclick={onClick} aria-controls="faq-entry-{id}" aria-expanded="{open ? 'true' : 'false'}" href="#{id}">
+      <i class="bi bi-chevron-{open ? 'up' : 'down'}"></i>
+      <h3>{title}</h3>
+    </a>
+  </header>
+  <div class="zm-collapsible-content" id="faq-entry-{id}" aria-hidden="{open ? 'false' : 'true'}">
+    {@html content}
+  </div>
+</article>
+<style>
+  .zm-collapsible {
+    overflow: hidden;
+    border: 1px solid var(--color-palette-secondary-30);
+    border-radius: var(--border-radius);
+
+    header {
+      padding: var(--spacing-s);
+    }
+  }
+
+  .zm-collapsible + .zm-collapsible {
+    margin-top: var(--spacing-s);
+  }
+
+  .zm-collapsible-content {
+    padding: var(--spacing-s);
+    border-top: 1px solid var(--color-palette-secondary-30);
+    background: var(--color-palette-secondary-10);
+  }
+
+  [aria-hidden='true'] {
+    display: none;
+  }
+</style>
