38931d8cce4778f5c6b6ba856d85749d446820b0
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2025-03-24 08:57:54 +0100

    Server mode in dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,36 @@ import paraglide from '@inlang/paraglide-astro';
 
 import svelte from '@astrojs/svelte';
 
+import node from '@astrojs/node';
+
+console.log(process.env.NODE_ENV);
+
 // https://astro.build/config
 export default defineConfig({
-  outDir: './public',
-  publicDir: './static',
-  i18n: {
-    locales: ['da', 'en', 'es', 'fi', 'fr', 'nb', 'sv'],
-    defaultLocale: 'en'
-  },
-  integrations: [
-    paraglide({
-      // recommended settings
-      project: './project.inlang',
-      outdir: './src/paraglide'
-    }),
-    svelte({
-      compilerOptions: {
-        customElement: true,
-      }
+    output: process.env.NODE_ENV === 'development' ? 'server' : 'static',
+
+    outDir: './public',
+    publicDir: './static',
+
+    i18n: {
+        locales: ['da', 'en', 'es', 'fi', 'fr', 'nb', 'sv'],
+        defaultLocale: 'en'
+    },
+
+    integrations: [
+        paraglide({
+            // recommended settings
+            project: './project.inlang',
+            outdir: './src/paraglide'
+        }),
+        svelte({
+            compilerOptions: {
+                customElement: true
+            }
+        })
+    ],
+
+    adapter: node({
+        mode: 'standalone'
     })
-  ]
 });
