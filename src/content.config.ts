d6e584c90ceb8bd0fe01ee78c179989b59ee5eb1
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2025-03-24 10:09:39 +0100

    Display faq by language

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const faqCollection = defineCollection({
+    type: 'content',
     schema: z.object({
         question: z.string(),
         category: z.string(),
