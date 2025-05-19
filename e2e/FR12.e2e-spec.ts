05be40e2b3487f56960440cc929995e7396c2544
Tobias Bleckert <hola@tobiasbleckert.se>
2025-05-19 17:27:41 +0200

    More test migration

--- e2e/FR12.e2e-spec.ts
+++ e2e/FR12.e2e-spec.ts
@@ -1,21 +1,20 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-import { goToHome, setLang, showOptions } from './utils/app.utils';
+import {goToHome, setLang, showOptions} from './utils/app.utils';
 
 test.describe('Zonemaster test FR12 - [The simple view should support an advanced view expanding when the checkbox is enabled]', () => {
-  test.beforeEach(async ({ page }) => {
-    await goToHome(page);
-    await setLang(page, 'en');
-  });
+    test.beforeEach(async ({page}) => {
+        await goToHome(page);
+        await setLang(page, 'en');
+    });
 
-  test('should have [Disable IPv4 checkbox] && [Disable IPv6 checkbox] NOT visible', async ({ page }) => {
-    await expect(page.locator('label[for="disable_protocol_ipv4"]')).toBeHidden();
-    await expect(page.locator('label[for="disable_protocol_ipv6"]')).toBeHidden();
-  });
+    test('should have [Disable IPv4 checkbox] && [Disable IPv6 checkbox] NOT visible', async ({page}) => {
+        await expect(page.locator('#advanced-options')).toHaveAttribute('hidden');
+    });
 
-  test('should have [Disable IPv4 checkbox] & [Disable IPv6 checkbox] visible', async ({ page }) => {
-    await showOptions(page);
-    await expect(page.locator('label[for="disable_protocol_ipv4"]')).toBeVisible();
-    await expect(page.locator('label[for="disable_protocol_ipv6"]')).toBeVisible();
-  });
+    test('should have [Disable IPv4 checkbox] & [Disable IPv6 checkbox] visible', async ({page}) => {
+        await showOptions(page);
+        await expect(page.locator('input[name="disabledIpType"][value="ipv4"]')).toBeVisible();
+        await expect(page.locator('input[name="disabledIpType"][value="ipv6"]')).toBeVisible();
+    });
 });
--- e2e/FR16.e2e-spec.ts
+++ e2e/FR16.e2e-spec.ts
@@ -1,32 +1,35 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-import { goToHome, setLang, showOptions } from './utils/app.utils';
+import {goToHome, setLang, showOptions} from './utils/app.utils';
 
 test.describe('Zonemaster test FR16 - [The advanced view should have a text describing what undelegated means]', () => {
-  test.beforeEach(async ({ page }) => {
-    await goToHome(page);
-    await setLang(page, 'en');
-    await showOptions(page);
-  });
+    test.beforeEach(async ({page}) => {
+        await goToHome(page);
+        await setLang(page, 'en');
+        await showOptions(page);
+    });
 
-  test('should have a link to the proper faq answer', async ({ page }) => {
-    const alert = page.locator('#advanced-options aside .alert');
-    await expect(alert).toBeVisible();
-    await expect(alert.locator('a')).toHaveAttribute('routerlink', '/faq');
-    await expect(alert.locator('a')).toHaveAttribute('fragment', 'what-is-an-undelegated-domain-test');
-  });
+    test('should have a link to the proper faq answer', async ({page}) => {
+        const alert = page.locator('#advanced-options div[role="alert"]');
+        await expect(alert).toBeVisible();
+        await expect(alert.locator('a')).toHaveAttribute('href', '/faq/#undelegated');
+    });
 
-  test('should have a description text in multi languages', async ({ page }) => {
-    const testSuite = [
-      { lang: 'en', text: 'undelegated' },
-      { lang: 'fr', text: 'non délégué' },
-      { lang: 'sv', text: 'odelegerat domäntest' },
-    ];
+    test('should have a description text in multi languages', async ({page}) => {
+        const testSuite = [
+            {lang: 'en', text: 'undelegated'},
+            {lang: 'fr', text: 'non délégué'},
+            {lang: 'sv', text: 'odelegerat domäntest'},
+        ];
 
-    for (const {lang, text} of testSuite) {
-      await setLang(page, lang);
-      await showOptions(page);
-      await expect(page.locator('#advanced-options aside .alert a')).toContainText(text);
-    }
-  });
+        for (const {lang, text} of testSuite) {
+            await setLang(page, lang);
+            await showOptions(page);
+            await page.waitForTimeout(400);
+
+            const alert = page.locator('#advanced-options div[role="alert"]');
+
+            await expect(alert.locator('a')).toContainText(text);
+        }
+    });
 });
