807a30becd03d30cd45e197f6820dd991f713758
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2025-06-04 12:01:36 +0200

    Read version from package.json

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 import type { Config } from '@/types.ts';
+import packageJson from './package.json';
 
 const config: Config = {
     defaultLanguage: 'en',
@@ -6,7 +7,7 @@ const config: Config = {
     apiBaseUrl: import.meta.env.PUBLIC_API_URL || '/api',
     pollingInterval: import.meta.env.PUBLIC_POLLING_INTERVAL || 5000,
     clientInfo: {
-        version: '5.0.0',
+        version: packageJson.version,
         id: 'Zonemaster-GUI',
     },
     siteInfo: {
