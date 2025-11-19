645e073fc6a04ef9325665d84827fea22e4c6c53
Mats Dufberg <mats.dufberg@iis.se>
2025-11-19 15:35:18 +0000

    Copies Apache configuration and license file to the zip file above the files served

--- scripts/create_release.js
+++ scripts/create_release.js
@@ -34,6 +34,18 @@ const inputDir = path.resolve(__dirname, '../public');
 const version = packageJson.version;
 const outputZip = path.resolve(__dirname, `../zonemaster_web_gui_${version}.zip`);
 
+// Copy zonemaster.conf-example into public
+const file1Src = path.resolve(__dirname, '../zonemaster.conf-example');
+const file1Dest = path.resolve(inputDir, 'zonemaster.conf-example');
+fs.copyFileSync(file1Src, file1Dest);
+console.log('Include zonemaster.conf-example in distribution zip file');
+
+// Copy LICENSE into public
+const file2Src = path.resolve(__dirname, '../LICENSE');
+const file2Dest = path.resolve(inputDir, 'LICENSE');
+fs.copyFileSync(file2Src, file2Dest);
+console.log('Include LICENSE in distribution zip file');
+
 zipDirectory(inputDir, outputZip)
     .then(() => console.log('Zip complete'))
     .catch(err => console.error('Error zipping:', err));
