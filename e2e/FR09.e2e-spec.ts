e5ed3d00ed1cad4f2054b809d17dfe3187c9e144
Mats Dufberg <mats.dufberg@iis.se>
2025-12-07 23:31:36 +0100

    Updates e2e test to match updated translation

--- e2e/FR09.e2e-spec.ts
+++ e2e/FR09.e2e-spec.ts
@@ -12,6 +12,6 @@ test.describe('Zonemaster test FR09 - [Once a language is chosen, all other link
         await expect(page.locator('input[name="domain"]')).toHaveAttribute('placeholder', 'Nom de domaine');
         await page.goto('/fr/faq/');
         await expect(page.locator('main h1')).toHaveText('Questions fréquentes');
-        await expect(page.locator('a.zm-menu__item[href$="/fr/"]')).toHaveText("Lancer un test");
+        await expect(page.locator('a.zm-menu__item[href$="/fr/"]')).toHaveText("Démarrer un test");
     });
 });
